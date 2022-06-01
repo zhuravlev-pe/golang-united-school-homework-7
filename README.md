46c336245601e349f9a72d83ec4ae6a0e0c0f884
NEKETSKY <nikitaverbovikov@gmail.com>
2022-06-01 22:55:50 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# golang-united-school-homework-7
+
+The goal of this homework is to write tests in `students_test.go` file and achieve as much coverage as possible.
+
+Inside this repo you can find 2 files and one directory:
+1. File `toBeTested.go` contains functions which should be tested. Do not change this file.
+2. File `students_test.go` is a boilerplate of file with your tests. At the start of this file you can find `init()` function, which is required because of peculiarities of Autocode platform. This function will copy your file to `/autocode` folder, do not touch this function.
+3. Folder `/autocode` to which your test will be automatically copied when you locally trigger your tests.
+
+Workflow to pass a homework: 
+1. Write tests in `students_test.go`
+2. Run your tests locally to check it status and resulted coverage. Each time you run tests your code will be copied to `/autocode` folder (NOTE: the name of this file is `students_test` - it's correct).
+3. Repeat steps 1 and 2 before you get desired result.
+4. When you will be ready to submit and test you solution on Autocode, you will need to commit and push `autocode/students_test` file to git. **Make sure that this file was automatically updated.** 
+
+After submitting of your solution on Autocode you will get some logs. If you faced with some error or did not achieve full coverage - modify your code and try again.
+
+Prerequisite:
+- Execute commands
+    - `go mod init`
+    - `go mod tidy`
---
+++ toBeTested.go
@@ -0,0 +1,101 @@
+package coverage
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Person struct {
+	firstName string
+	lastName  string
+	birthDay  time.Time
+}
+
+//People is a slice of type person
+type People []Person
+
+//Len returns the length of input value
+func (p People) Len() int {
+	return len(p)
+}
+
+//Less reports when it is necessary to sort element i before element j
+func (p People) Less(i, j int) bool {
+	if p[i].birthDay.Unix() == p[j].birthDay.Unix() {
+		if p[i].firstName == p[j].firstName {
+			return p[i].lastName < p[j].lastName
+		}
+		return p[i].firstName < p[j].firstName
+	}
+	return p[i].birthDay.Unix() > p[j].birthDay.Unix()
+}
+
+//Swap swaps the elements with indexes i and j.
+func (p People) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Matrix stores an array of numbers in rows and columns
+type Matrix struct {
+	rows, cols int
+	data       []int
+}
+
+/*New creates a matrix from a string.*/
+func New(str string) (*Matrix, error) {
+	rows := strings.Split(str, "\n")
+	matrix := Matrix{rows: len(rows), cols: -1}
+	for _, row := range rows {
+		cols := strings.Split(strings.TrimSpace(row), " ")
+		if matrix.cols == -1 {
+			matrix.cols = len(cols)
+		} else if matrix.cols != len(cols) {
+			return nil, fmt.Errorf("Rows need to be the same length")
+		}
+		for _, char := range cols {
+			num, err := strconv.Atoi(char)
+			if err != nil {
+				return nil, err
+			}
+			matrix.data = append(matrix.data, num)
+		}
+	}
+	return &matrix, nil
+}
+
+/*Rows gets the matrix represented in rows.*/
+func (m Matrix) Rows() [][]int {
+	rows := make([][]int, m.rows)
+	for r := 0; r < m.rows; r++ {
+		rows[r] = make([]int, m.cols)
+		for c := 0; c < m.cols; c++ {
+			rows[r][c] = m.data[r*m.cols+c]
+		}
+	}
+	return rows
+}
+
+/*Cols gets the matrix represented in columns*/
+func (m Matrix) Cols() [][]int {
+	cols := make([][]int, m.cols)
+	for c := 0; c < m.cols; c++ {
+		cols[c] = make([]int, m.rows)
+		for r := 0; r < m.rows; r++ {
+			cols[c][r] = m.data[r*m.cols+c]
+		}
+	}
+	return cols
+}
+
+/*Set sets the value of the matrix at point row, col.*/
+func (m *Matrix) Set(row, col, value int) bool {
+	if row < 0 || m.rows <= row || col < 0 || m.cols <= col {
+		return false
+	}
+	m.data[row*m.cols+col] = value
+	return true
+}
